3c369ec4c822461d206b2a6c0916e1b4a1ec8c02
Lincoln Sanderson <lincoln@paperton.com>
2012-02-06 15:43:36 +0100

    Added babes type. Still need moar, or a random image generator.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -15,7 +15,7 @@ class ImageController < ApplicationController
   private
 
   def getters
-    ['pug','placeholder','kitten','zombie']
+    ['pug','placeholder','kitten','zombie', 'babe']
   end
 
   def get_image
@@ -61,8 +61,35 @@ class ImageController < ApplicationController
   end
 
   def get_babe
-    #THIS HAS TO HAPPEN
-    raise ActiveRecord::RecordNotFound
+    babes = [
+      "http://2.bp.blogspot.com/_XmYwA_GdPeo/TJKcvCGlA2I/AAAAAAAAJ9M/7Bz5lkgFwLA/s1600/miranda-kerr-swim-0310-4.jpg",
+      "http://img.jesper.nu/view/6819/miranda-kerr-0111-05.jpg",
+      "http://blog.modelmanagement.com/library/uploads/alexandra-ambrosia-for-victorias-secret-catalouge-20091.jpg",
+      "http://cornersmagazine.com/wp-content/uploads/2011/11/miranda-kerr-victoria-secret.jpg",
+      "http://www.activedollhouse.com/wp-content/gallery/maxim-cover-march-2009/maxim4.jpg",
+      "http://starsmedia.ign.com/stars/image/article/998/998906/babe-of-the-week-062609-20090626035641059-000.jpg",
+      "http://headblitz.com/wp-content/uploads/2010/10/Busty-Babe.jpg",
+      "http://iphonetoolbox.com/wp-content/uploads/2008/08/hot-babe-07-f.jpg",
+      "http://www.runningwithscissors.com/images/postal-babes/belinda-strange/postal-babe-belinda-strange-20.jpg",
+      "http://www.jpegwallpapers.com/images/wallpapers/Babe-18-241499.jpeg",
+      "http://starsmedia.ign.com/stars/image/article/827/827049/babe-of-the-week-101207-20071012034114041-000.jpg",
+      "http://www.ridelust.com/wp-content/uploads/car_girl_tuner_car.jpg",
+      "http://1.bp.blogspot.com/_QQ9atl4wcXI/TF6FHvjqxAI/AAAAAAAAApM/GhIgT7N8Hi8/s1600/babe-of-the-week-080108-20080801014859705-000.jpg",
+      "http://www.strangecelebrities.com/images/content/125738.jpg",
+      "http://1.bp.blogspot.com/-3l7ytzWTqsw/TkIoMJIBIPI/AAAAAAAAAIQ/Cy6owDC0S7U/s1600/sexy_lucy_pinder_black%2B7.jpg",
+      "http://girlnight.info/Sexy_Girl_At_Night__9__b.jpg",
+      "http://thaiintelligentnews.files.wordpress.com/2010/11/victoria-s-secret-very-sexy-topless-bikini_sexy-victorias-secret-swimsuits.jpg",
+      "http://1.bp.blogspot.com/-FGLBXqdvG9k/Th3h7RPBpWI/AAAAAAAAH9w/S3MQjw-03eA/s1600/Sexy+Image+nurse.jpg",
+      "http://4.bp.blogspot.com/-VJxfeU0vCI4/Th3h3Fi7mYI/AAAAAAAAH9U/8SLQyr2NQZs/s1600/ayesha+Sexy+Image.jpg",
+      "http://fr.flash-screen.com/free-wallpaper/uploads/200603/imgs/1141532161_1024x768_jessica-alba-photo-jessica-alba-sexy-photo.jpg",
+      "http://www.pulsarmedia.ro/data/media/19/Nikki%20Visser%201024X768%202822%20Sexy%20Wallpaper.jpg",
+      "http://3.bp.blogspot.com/-76wqSGJhDLw/Th3h9Tt9NbI/AAAAAAAAH94/CaLiZPawcno/s1600/Sexy+Image+singles.jpg",
+    ]
+    #NEED MOOOOOOAR
+    
+    @file = open(babes[rand babes.length])
+    @image = MiniMagick::Image.read(@file.read)
+    resize_image
   end
 
   def get_zombie
